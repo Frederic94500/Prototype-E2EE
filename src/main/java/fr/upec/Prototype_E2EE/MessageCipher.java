6da55cda190c77a7244a1220ace79221491ecb7c
mialy <mialysandy@gmail.com>
2022-11-14 17:48:50 +0100

    Cipher prototype

--- src/main/java/fr/upec/Prototype_E2EE/MessageCipher.java
+++ src/main/java/fr/upec/Prototype_E2EE/MessageCipher.java
@@ -1,11 +1,49 @@
 package fr.upec.Prototype_E2EE;
 
-public class MessageCipher {
-    public static byte[] cipher() {
-        //Encrypt with AES(256) + GCM, return as byte[]
-    }
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
-    public static byte[] decipher() {
-        //Decrypt, return as byte[] or String, idk ¯\_(ツ)_/¯
+public class MessageCipher {
+
+    public static final int GCM_IV_LENGTH = 12;
+    public static final int GCM_TAG_LENGTH = 16;
+    public static byte[] cipher(byte[] plaintext, SecretKey key) throws Exception
+    {
+        // Get Cipher Instance
+        Cipher cipher = Cipher.getInstance("AES_256/GCM/NoPadding");
+
+        // Create SecretKeySpec
+       // SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
+
+        // Create GCMParameterSpec
+        //GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
+
+        // Initialize Cipher for ENCRYPT_MODE
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        // Perform Encryption
+        byte[] cipherText = cipher.doFinal(plaintext);
+
+        return cipherText;
+    }
+    public static byte[] decipher(byte[] cipherText, SecretKey key) throws Exception
+    {
+        // Get Cipher Instance
+        Cipher cipher = Cipher.getInstance("AES_256/GCM/NoPadding");
+
+        // Create SecretKeySpec
+       // SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
+
+        // Create GCMParameterSpec
+       // GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
+
+        // Initialize Cipher for DECRYPT_MODE
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        // Perform Decryption
+        return cipher.doFinal(cipherText);
+        //return new String(decryptedText);
     }
 }
