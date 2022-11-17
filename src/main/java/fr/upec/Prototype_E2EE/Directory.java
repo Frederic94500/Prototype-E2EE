3ab31e3e666dca14215ec7b08feac95fdb507548
mialy <mialysandy@gmail.com>
2022-11-17 10:31:41 +0100

    directory version 2

--- src/main/java/fr/upec/Prototype_E2EE/Directory.java
+++ src/main/java/fr/upec/Prototype_E2EE/Directory.java
@@ -1,23 +1,20 @@
 package fr.upec.Prototype_E2EE;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 
 public class Directory {
     final static String path = new File("directory").getAbsolutePath();
     public HashMap<String, byte[]> directory;
 
-    public Directory(HashMap<String, byte[]> directory) throws IOException {
+    public Directory() throws IOException {
 
         File f = new File(path);
         if (f.exists()) {
             //FileInputStream file = new FileInputStream(path);
-            directory = hashMapFromTextFile();
+            this.directory = hashMapFromTextFile();
         } else {
-
+            FileInputStream file = new FileInputStream(path);
 
         }
 
@@ -25,10 +22,9 @@ public class Directory {
     }
 
     public static HashMap<String, byte[]> hashMapFromTextFile() throws IOException {
-        HashMap<String, byte[]> map = new HashMap<String, byte[]>();
+        HashMap<String, byte[]> map = new HashMap<>();
         File file = new File(path);
-        BufferedReader br = null;
-        br = new BufferedReader(new FileReader(file));
+        BufferedReader br = new BufferedReader(new FileReader(file));
         String line = null;
         while ((line = br.readLine()) != null) {
             String[] separated = line.split(":");
