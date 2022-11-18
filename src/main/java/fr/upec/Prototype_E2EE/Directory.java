c9462600ad17e0dff565f29664cc9073be62099b
Frédéric TRAN <frederictran@hotmail.fr>
2022-11-18 23:51:27 +0100

    Added isInDirectory method

--- src/main/java/fr/upec/Prototype_E2EE/Directory.java
+++ src/main/java/fr/upec/Prototype_E2EE/Directory.java
@@ -1,24 +1,22 @@
 package fr.upec.Prototype_E2EE;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Directory {
     final static String path = new File("directory").getAbsolutePath();
     public HashMap<String, byte[]> directory;
 
     public Directory() throws IOException {
-
         File f = new File(path);
         if (f.exists()) {
             //FileInputStream file = new FileInputStream(path);
             this.directory = hashMapFromTextFile();
         } else {
             FileInputStream file = new FileInputStream(path);
-
         }
-
-
     }
 
     public static HashMap<String, byte[]> hashMapFromTextFile() throws IOException {
@@ -34,9 +32,23 @@ public class Directory {
             if (!name.equals("") && !pubKey.equals("")) {
                 map.put(name, bytePubKey);
             }
-
         }
         br.close();
         return map;
+    }
+
+    /**
+     * Search in Directory if the otherPubKey is present
+     *
+     * @param otherPubKey The otherPubKey
+     * @return Return a boolean if it is present
+     */
+    public boolean isInDirectory(byte[] otherPubKey) {
+        for (Map.Entry<String, byte[]> entry : directory.entrySet()) {
+            if (Arrays.equals(otherPubKey, entry.getValue())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
