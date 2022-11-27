3f8bec78a3d7d22a68aef6c306f74606d2d9ad46
mialy <mialysandy@gmail.com>
2022-11-27 12:30:04 +0100

    bon le commit du save j'ai testé normalment il est bon j'ai considéré que le pubkey quand il est généré c des string qu'on encode en byte sur une bas64

--- src/main/java/fr/upec/Prototype_E2EE/Directory.java
+++ src/main/java/fr/upec/Prototype_E2EE/Directory.java
@@ -2,6 +2,7 @@ package fr.upec.Prototype_E2EE;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,28 +14,49 @@ public class Directory {
         File f = new File(path);
         if (f.exists()) {
             //FileInputStream file = new FileInputStream(path);
-            this.directory = hashMapFromTextFile();
+            this.directory = textFileToHashMap();
         } else {
             FileInputStream file = new FileInputStream(path);
         }
     }
 
-    public static HashMap<String, byte[]> hashMapFromTextFile() throws IOException {
+    public static HashMap<String, byte[]> textFileToHashMap() throws IOException {
         HashMap<String, byte[]> map = new HashMap<>();
         File file = new File(path);
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        BufferedReader br = new BufferedReader(new FileReader(file)); // yves:OUGLIYGjhmiugflggliyfliyful
         String line = null;
         while ((line = br.readLine()) != null) {
             String[] separated = line.split(":");
             String name = separated[0].trim();
             String pubKey = separated[1].trim();
             byte[] bytePubKey = pubKey.getBytes();
-            if (!name.equals("") && !pubKey.equals("")) {
+            if (!name.equals("") && !pubKey.equals("")) {//"c/mialy
                 map.put(name, bytePubKey);
             }
         }
         br.close();
         return map;
+    }
+    //je ne vois l'interet de save en bas64 ça rajoute juste du travail;
+    public static void saveIntoDirectory(String name,byte[] pubkey) throws IOException{
+
+        File f = new File("directory.txt");
+        f.createNewFile();
+        if (f.exists()) {
+            String encodedString = Base64.getEncoder().encodeToString(name.getBytes());
+            String encodepubkey=Base64.getEncoder().encodeToString(pubkey);
+            FileWriter fw = new FileWriter("directory.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(encodedString);
+            bw.write(":");
+            bw.write(encodepubkey);
+            bw.newLine();
+            bw.close();
+            System.out.println("écriture dans fichier...");
+        } else {
+
+            System.out.println("creation fichier...");
+        }
     }
 
     /**
@@ -50,5 +72,8 @@ public class Directory {
             }
         }
         return false;
+    }
+    public static void saveIntoDirectory(){
+
     }
 }
